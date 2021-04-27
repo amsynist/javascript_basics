916f0dcf843439bd387ae68f982c2375a3915c8c
amsynist <pravinblogging@gmail.com>
2021-04-27 19:58:10 +0530

    new

---
+++ browser_object_model.js
@@ -0,0 +1,14 @@
+//BOM --> Browser_Object_Model
+//Browser objects are global objects available throughout the js code.
+
+//Types of BOM objects are:
+
+// 1.window object:{ --> windows object has 5 objects are labelled as A,B,C,D,E
+//                    (A)location object:{}
+//                    (B)history object:{}
+//                    (C)document object:{ --> document object has again 3 objects are labelled as a,b,c
+//                    (a) forms object:{},(b)images object{},(c)links object:{}
+//                     }
+//                    (D)screen object:{}
+//                    (E)navigator object:{}
+//                                                      }
